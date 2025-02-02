8bdc34aa138b960878e8ceb25f7e0a8c0dbc17cf
LucasMtss <lucasrmts@gmail.com>
2025-02-02 15:20:33 -0300

    fix: fix image path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,12 +20,18 @@ export const metadata: Metadata = {
     description: "Venha comemorar o aniversário da Vó Zezé!",
     images: [
       {
-        url: "public/share-image.jpg",
+        url: "/share-image.jpg",
         width: 800,
         height: 600,
         alt: "Aniversário da Vó Zezé",
       },
     ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Aniversário da Vó Zezé",
+    description: "Venha comemorar o aniversário da Vó Zezé!",
+    images: ["/share-image.jpg"],
   },
 };
 
@@ -39,7 +45,7 @@ export default function RootLayout({
       <head>
         <meta property="og:title" content="Aniversário da Vó Zezé" />
         <meta property="og:description" content="Venha comemorar o aniversário da Vó Zezé!" />
-        <meta property="og:image" content="public/share-image.jpg" />
+        <meta property="og:image" content="/share-image.jpg" />
         <meta property="og:image:width" content="800" />
         <meta property="og:image:height" content="600" />
       </head>
